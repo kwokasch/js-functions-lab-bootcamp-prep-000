a13306f7496835fd991ca828d42ea9555e6ce685
Katie Wokasch <katie.wokasch@gmail.com>
2019-07-20 14:22:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`
 }
 
-function happyHolidaysTo(holiday, name){
+function happyHolidayTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(name){
   return `Happy holidays, ${name}!`
 }
